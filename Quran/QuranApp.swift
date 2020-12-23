86913c8f9fc83cfbc7739ef0f1ac1c0bf70c46af
Kamil <rehaurd@mail.ru>
2020-12-24 00:50:38 +0300

    Initial Commit

---
+++ Quran/QuranApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranApp.swift
+//  Quran
+//
+//  Created by Камиль  Сулейманов on 24.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct QuranApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
